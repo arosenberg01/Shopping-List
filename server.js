b13a764aa1fa2d6fac864fb8d91ba15db260168f
MrBreadcrumbs <ansel01@gmail.com>
2015-01-02 23:01:03 -0800

    Add on-page POSTing

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var http = require('http');
 var url = require('url');
-var items = [];
+var join = require('path').join;
+var fs = require('fs');
+var qs = require('querystring');
+var parse = require('url').parse;
+
+var items = ['milk', 'cheese'];
+var root = __dirname;	
 
 var server = http.createServer(function (req, res) {
 	switch (req.method) {
@@ -11,15 +17,24 @@ var server = http.createServer(function (req, res) {
 				item += chunk;
 			});
 			req.on('end', function() {
-				items.push(item);
-				res.end('Item added\n');
+				var obj = qs.parse(item);
+				items.push(obj.item);
+				console.log(items);
+				
+			list = '';
+			items.forEach(function (item, i) {
+				list += "<li>" + item + "</li>";
+			})
+			res.write("<!DOCTYPE html><html><head><title>Shopping List</title></head><body><form method=\"post\"><input type=\"text\" name=\"item\" placeholder=\"Enter an item\"><button>Add Item</button></form><h2>Shopping List</h2><ul id=\"list\">" + list + "</ul></body></html>");
+			res.end();
 			});
 			break;
 		case 'GET':
+			list = '';
 			items.forEach(function (item, i) {
-				res.write(i + '. ' + item + '\n');
+				list += "<li>" + item + "</li>";
 			});
-			res.end();
+			res.write("<!DOCTYPE html><html><head><title>Shopping List</title></head><body><form method=\"post\"><input type=\"text\" name=\"item\" placeholder=\"Enter an item\"><button>Add Item</button></form><h2>Shopping List</h2><ul id=\"list\">" + list + "</ul></body></html>");	
 			break;
 		case 'DELETE':
 			var pathname = url.parse(req.url).pathname;
@@ -29,7 +44,7 @@ var server = http.createServer(function (req, res) {
 				res.end('Item id not valid');
 			} else if (!items[i]) {
 				res.statusCode = 404;
-				res.end('Tem not found');
+				res.end('Item not found');
 			} else {
 				items.splice(i, 1);
 				res.end('Item deleted successfully');
@@ -58,6 +73,29 @@ var server = http.createServer(function (req, res) {
 			break;
 			
 	}
+
+	// var url = parse(req.url);
+	// var path = join(root, url.pathname);
+	// fs.stat(path, function (err, stat) {
+ //        if (err) {
+ //            if (err.code == 'ENOENT') {
+ //                res.statusCode = 404;
+ //                res.end('File Not Found');
+ //            }
+ //            else {
+ //                res.statusCode = 500;
+ //                res.end('Internal Server Error1');
+ //            }
+ //        } else {
+ //            var stream = fs.createReadStream(path);
+ //            // res.setHeader('Content-Length', stat.size);
+ //            stream.pipe(res);
+ //            stream.on('error', function (err) {
+ //                res.statusCode = 500;
+ //                res.end('Internal Server Error2');
+ //            });
+ //        }
+ //    });
 });
 
 server.listen(9000, function() {
